4a3cb7913d603b5b12598b0bcab02206d26c4aa3
Sirbu Valentin <sirbuvalentin@aol.com>
2023-04-24 01:02:36 +0300

    Inceracre de autoinstalare pip

--- gui.py
+++ gui.py
@@ -1,22 +1,3 @@
-
-
-
-
-import urllib.request
-import subprocess
-
-# Adresa URL pentru fișierul get-pip.py
-url = "https://bootstrap.pypa.io/get-pip.py"
-
-# Numele fișierului local pentru descărcare
-filename = "get-pip.py"
-
-# Descărcarea fișierului get-pip.py
-urllib.request.urlretrieve(url, filename)
-
-# Rularea fișierului get-pip.py pentru instalarea pip
-subprocess.call(["python", filename])
-
 import PySimpleGUI as sg
 import b_and as be
 
